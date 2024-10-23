525791fb5e19c0ca0fdf9835ed22dd5ca56329a1
Toni Tesori <tt@tonitesori.dev>
2024-10-23 10:46:45 -0600

    Add transaction to DB

--- components/AddTransaction.tsx
+++ components/AddTransaction.tsx
@@ -1,23 +1,24 @@
 'use client';
 import addTransaction from '@/app/actions/addTransaction';
-import React from 'react';
+import React, { useRef } from 'react';
 import { toast } from 'react-toastify';
 
 const AddTransaction = () => {
+  const formRef = useRef<HTMLFormElement>(null);
   const clientAction = async (formData: FormData) => {
     const { data, error } = await addTransaction(formData);
 
     if (error) {
       toast.error(error);
     } else {
-      alert('Transaction added');
-      console.log(data);
+      toast.success('Transaction added');
+      formRef.current?.reset();
     }
   };
   return (
     <>
       <h3>Add Transaction</h3>
-      <form action={clientAction}>
+      <form action={clientAction} ref={formRef}>
         <div className="form-control">
           <label htmlFor="text">Text</label>
           <input type="text" id="text" name="text" placeholder="Enter text" />
