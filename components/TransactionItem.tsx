b9bdb8d3bf53117c310ed96709432d0b8257d730
Toni Tesori <tt@tonitesori.dev>
2024-10-23 13:14:02 -0600

    Added delete transaction

--- components/TransactionItem.tsx
+++ components/TransactionItem.tsx
@@ -1,17 +1,31 @@
+'use client';
 import { Transaction } from '@/types/Transaction';
 import { addCommas } from '@/lib/utils';
 import { toast } from 'react-toastify';
+import deleteTransaction from '@/app/actions/deleteTransaction';
 
 import React from 'react';
 
 const TransactionItem = ({ transaction }: { transaction: Transaction }) => {
   const sign = transaction.amount < 0 ? '-' : '+';
+  const handleDeleteTransaction = async (id: string) => {
+    const confirmed = window.confirm('Are you sure you want to delete?');
+    if (!confirmed) return;
+    await deleteTransaction(id);
+    toast.success('Transaction deleted.');
+  };
   return (
     <li className={transaction.amount < 0 ? 'minus' : 'plus'}>
       {transaction.text}{' '}
       <span>
         {sign} {addCommas(Math.abs(transaction.amount))}
       </span>
+      <button
+        onClick={() => handleDeleteTransaction(transaction.id)}
+        className="delete-btn"
+      >
+        x
+      </button>
     </li>
   );
 };
