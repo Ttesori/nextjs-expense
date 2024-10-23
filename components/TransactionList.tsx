a284078e5495d86c6966a8deacb2d48763fa8b56
Toni Tesori <tt@tonitesori.dev>
2024-10-23 12:09:34 -0600

    Add transaction list

---
+++ components/TransactionList.tsx
@@ -0,0 +1,25 @@
+import { Transaction } from '@/types/Transaction';
+import getTransactions from '@/app/actions/getTransactions';
+
+const TransactionList = async () => {
+  const { transactions, error } = await getTransactions();
+
+  if (error) {
+    return <p className="error">{error}</p>;
+  }
+  return (
+    <>
+      <h3>History</h3>
+      <ul className="list">
+        {transactions &&
+          transactions.map((transaction: Transaction) => (
+            <li>
+              {transaction.text} : {transaction.amount}
+            </li>
+          ))}
+      </ul>
+    </>
+  );
+};
+
+export default TransactionList;
